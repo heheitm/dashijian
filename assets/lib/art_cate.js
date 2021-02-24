228710e931d0f659284705a12671e1055c45c3d6
heyonghua <2274261497@qq.com>
2021-02-24 20:16:59 +0800

    第三次提交

---
+++ assets/lib/art_cate.js
@@ -0,0 +1,54 @@
+$(function () {
+    initArtCateList()
+
+    // 获取文章分类的列表
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res);
+                $('tbody').html(htmlStr)
+            }
+        })
+    };
+    //
+    var layer = layui.layer;
+    //声明一个变量
+    var indexIlay=null;
+    $('#btnAddCate').on('click', function () {
+        //弹出层
+        indexIlay=layer.open({
+            type: 1,
+            area: ['500px', '300px'],
+            title: '添加类别',
+            //渲染弹出层
+            content: $('#dialog-add').html()
+        })
+    });
+    // 通过代理的形式，为 form-add 表单绑定 submit 事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        const str = $(this).serialize();
+        $.post('/my/article/addcates', str, function (res) {
+            if(res.status !== 0){
+                return layer.msg('新增失败')
+            }
+            layer.msg('新增成功');
+            initArtCateList();
+            //根据索引删除 弹出层
+            layer.close(indexIlay);
+        })
+    });
+    var indexIlayer=null;
+    $('tbody').on('click','.btn-edit',function(){
+        indexIlayer=layer.open({
+            type: 1,
+            area: ['500px', '300px'],
+            title: '修改文章分类',
+            //渲染弹出层
+            content: $('#dialog-edit').html()
+        })
+    });
+    
+})
